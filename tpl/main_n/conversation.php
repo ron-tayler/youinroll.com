cdb79573124543be4a33980791aaea6aa7f0c3be
Ron Tayler <max.urs@yandex.ru>
2021-05-28 03:33:23 +0300

    Фикс аватарки справа-сверху на странице чата

--- tpl/main_n/conversation.php
+++ tpl/main_n/conversation.php
@@ -12,13 +12,7 @@
             <div class="chat-area-header">
                 <div class="chat-area-title">Выберите чат</div>
                 <div class="chat-area-group">
-                    <img class="chat-area-profile" src="" alt="" />
-                    <span></span>
-                    <!-- <img class="chat-area-profile"
-                        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%282%29.png" alt="">
-                    <img class="chat-area-profile"
-                        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%2812%29.png" alt="" />
-                     -->
+                    <span data-toggle="modal" data-target="#addParticipantModal" id="addParticipant">+</span>
                 </div>
             </div>
             <div class="chat-area-main">
